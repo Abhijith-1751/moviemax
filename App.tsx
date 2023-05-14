c47e163dd7da369338baf1918bfb687b2a8d79c9
Abhijith-1751 <72120509+Abhijith-1751@users.noreply.github.com>
2023-05-14 11:55:50 +0530

    1 binaries uploaded

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
+import Navbar from './components/navbar';
 import './style.css';
 
 export default function App() {
   return (
     <div>
-      
+      <Navbar/>
     </div>
   );
 }
