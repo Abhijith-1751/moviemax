e1c88425e23ad3024e9c99641d6853b279878bd4
Abhijith-1751 <72120509+Abhijith-1751@users.noreply.github.com>
2023-05-14 12:09:57 +0530

    2 files modified

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,18 @@
 import * as React from 'react'
 import '../css/navbar.css'
 
+
 function Navbar(){
   return(
    <nav>
-
+    <img src="https://stackblitz.com/files/moviemax-io/github/Abhijith-1751/moviemax/main/img/logo.png" alt="Logo"/>
+    
+    <ul>
+      <button>Home</button>
+      <button>Kids</button>
+      <button>Action</button>
+      <button>Romantic</button>
+    </ul>
 
    </nav>
   );
